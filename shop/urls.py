5f94a133ace5c5174cd6c9e3589b3df50da0c09a
Rasul0819 <rasulabdikerimov87@gmail.com>
2025-11-24 14:32:10 +0500

    search ham kitaplar qosildi

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('search/', views.search_results, name='search_results'),
+    path('popular-books/', views.popular_books, name='popular_books'),
     path('book/<int:book_id>/', views.book_detail, name='detail'),
     ]
--- shop/views.py
+++ shop/views.py
@@ -2,11 +2,11 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Book, Genres, Authors, Languages, Countries, Review, ReviewImage, CustomUser
 # Create your views here.
-
+from django.db.models import Q
 def homepage(request):
-    books = Book.objects.all()
-    genres = Genres.objects.all()
-    authors = Authors.objects.all()
+    books = Book.objects.all()[:4]
+    genres = Genres.objects.all()[:6]
+    authors = Authors.objects.all()[:6]
     context = {
         'books': books,
         'genres': genres,
@@ -14,6 +14,27 @@ def homepage(request):
     }
     return render(request, 'shop/homepage.html', context)
 
+def popular_books(request):
+    books = Book.objects.all()[:20]
+    context = {
+        'books': books,
+    }
+    return render(request, 'shop/popular_books.html', context)
+
+
+def search_results(request):
+    query = request.GET.get('q', '')
+
+    print(query)
+    books = Book.objects.filter(
+        Q(title__icontains=query)|
+        Q(authors__full_name__icontains=query)
+        ).distinct()
+    context = {
+        'books': books,
+        'query': query,
+    }
+    return render(request, 'shop/search_results.html', context)
 def book_detail(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     reviews = Review.objects.filter(book=book).select_related('user')
